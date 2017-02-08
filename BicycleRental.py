1bb1bda8ca5cf29444234e2b82c212eb4aef7b6b
roger <rogeryu23@outlook.com>
2017-02-07 20:22:56 -0800

    Adding the NeuralNetwork2.py to demostrate a different way to calculate weight which is from output -> input. and also checkin the python notebook

--- BicycleRental.py
+++ BicycleRental.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sys
-from NeuralNetwork import NeuralNetwork
-from NeuralNetwork import MSE
+from NeuralNetwork2 import NeuralNetwork
+from NeuralNetwork2 import MSE
 
 data_path = 'Bike-Sharing-Dataset/hour.csv'
 rides = pd.read_csv(data_path)
@@ -42,7 +42,7 @@ train_features, train_targets = features[:-60*24], targets[:-60*24]
 val_features, val_targets = features[-60*24:], targets[-60*24:]
 
 # Set the hyper parameters here ###
-epochs = 100
+epochs = 1000
 learning_rate = 0.1
 hidden_nodes = 5
 output_nodes = 1
@@ -79,7 +79,9 @@ fig, ax = plt.subplots(figsize=(8,4))
 
 mean, std = scaled_features['cnt']
 predictions = network.run(test_features)*std + mean
-ax.plot(predictions, label='Prediction')
+
+# If using NeuralNetwork.py, then the predictions[0] should be changed to predictions
+ax.plot(predictions[0], label='Prediction')
 ax.plot((test_targets['cnt']*std + mean).values, label='Data')
 ax.set_xlim(right=len(predictions))
 ax.legend()
@@ -88,3 +90,60 @@ dates = pd.to_datetime(rides.ix[test_data.index]['dteday'])
 dates = dates.apply(lambda d: d.strftime('%b %d'))
 ax.set_xticks(np.arange(len(dates))[12::24])
 _ = ax.set_xticklabels(dates[12::24], rotation=45)
+
+# Unit test
+import unittest
+
+inputs = [0.5, -0.2, 0.1]
+targets = [0.4]
+test_w_i_h = np.array([[0.1, 0.4, -0.3],
+                       [-0.2, 0.5, 0.2]])
+test_w_h_o = np.array([[0.3, -0.1]])
+
+
+class TestMethods(unittest.TestCase):
+    ##########
+    # Unit tests for data loading
+    ##########
+
+    def test_data_path(self):
+        # Test that file path to dataset has been unaltered
+        self.assertTrue(data_path.lower() == 'bike-sharing-dataset/hour.csv')
+
+    def test_data_loaded(self):
+        # Test that data frame loaded
+        self.assertTrue(isinstance(rides, pd.DataFrame))
+
+    ##########
+    # Unit tests for network functionality
+    ##########
+
+    def test_activation(self):
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        # Test that the activation function is a sigmoid
+        self.assertTrue(np.all(network.activation_function(0.5) == 1 / (1 + np.exp(-0.5))))
+
+    def test_train(self):
+        # Test that weights are updated correctly on training
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        network.train(inputs, targets)
+        self.assertTrue(np.allclose(network.weights_hidden_to_output,
+                                    np.array([[0.37275328, -0.03172939]])))
+        self.assertTrue(np.allclose(network.weights_input_to_hidden,
+                                    np.array([[0.10562014, 0.39775194, -0.29887597],
+                                              [-0.20185996, 0.50074398, 0.19962801]])))
+
+    def test_run(self):
+        # Test correctness of run method
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        self.assertTrue(np.allclose(network.run(inputs), 0.09998924))
+
+
+suite = unittest.TestLoader().loadTestsFromModule(TestMethods())
+unittest.TextTestRunner().run(suite)
--- NeuralNetwork2.py
+++ NeuralNetwork2.py
@@ -5,7 +5,7 @@ def MSE(y, Y):
     return np.mean((y - Y) ** 2)
 
 
-# This NeuralNetwork weight is define as from input -> output direction
+# This NeuralNetwork weight is define as from output -> input direction
 class NeuralNetwork(object):
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         # Set number of nodes in input, hidden and output layers.
@@ -14,28 +14,29 @@ class NeuralNetwork(object):
         self.output_nodes = output_nodes
 
         # Initialize weights
-        self.weights_input_to_hidden = np.random.normal(0.0, self.input_nodes ** -0.5,
-                                                        (self.input_nodes, self.hidden_nodes))
+        self.weights_input_to_hidden = np.random.normal(0.0, self.hidden_nodes**-0.5,
+                                       (self.hidden_nodes, self.input_nodes))
 
-        self.weights_hidden_to_output = np.random.normal(0.0, self.hidden_nodes ** -0.5,
-                                                         self.hidden_nodes)
+        self.weights_hidden_to_output = np.random.normal(0.0, self.output_nodes**-0.5,
+                                       (self.output_nodes, self.hidden_nodes))
         self.lr = learning_rate
 
         # Activation function is the sigmoid function
         self.activation_function = lambda x: 1 / (1 + np.exp(-x))
 
     def train(self, inputs_list, targets_list):
-        inputs = inputs_list
-        targets = targets_list
+        # Convert inputs list to 2d array
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
 
         # Implement the forward pass here
         # Forward pass ###
         # Hidden layer
-        hidden_inputs = np.dot(inputs, self.weights_input_to_hidden)
+        hidden_inputs = np.dot(self.weights_input_to_hidden, inputs)
         hidden_outputs = self.activation_function(hidden_inputs)
 
         # Output layer
-        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output)
+        final_inputs = np.dot(self.weights_hidden_to_output, hidden_outputs)
         final_outputs = final_inputs
 
         # Implement the backward pass here
@@ -46,24 +47,25 @@ class NeuralNetwork(object):
         output_errors = targets - final_outputs
 
         # Calculate error gradient in hidden units
-        hidden_errors = np.dot(self.weights_hidden_to_output, output_errors)
-        hidden_grad = hidden_errors * hidden_outputs * (1 - hidden_outputs)
+        hidden_errors = np.dot(output_errors, self.weights_hidden_to_output)
+        hidden_grad = hidden_errors.T * hidden_outputs * (1 - hidden_outputs)
 
         # Update the weights
-        self.weights_hidden_to_output += self.lr * output_errors * hidden_outputs
-        self.weights_input_to_hidden += self.lr * hidden_grad * inputs[:, None]
+        self.weights_hidden_to_output += self.lr * np.dot(output_errors, hidden_outputs.T)
+        self.weights_input_to_hidden += self.lr * np.dot(hidden_grad, inputs.T)
 
     def run(self, inputs_list):
-        # Run a forward pass through the network
-        inputs = inputs_list
+        # Convert inputs list to 2d array
+        inputs = np.array(inputs_list, ndmin=2).T
 
         # Implement the forward pass here
+        # Forward pass ###
         # Hidden layer
-        hidden_inputs = np.dot(inputs, self.weights_input_to_hidden)
+        hidden_inputs = np.dot(self.weights_input_to_hidden, inputs)
         hidden_outputs = self.activation_function(hidden_inputs)
 
         # Output layer
-        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output)
+        final_inputs = np.dot(self.weights_hidden_to_output, hidden_outputs)
         final_outputs = final_inputs
 
         return final_outputs
